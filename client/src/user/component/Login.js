44ff50967f9bac1778b869df4358e6061698a230
NITISH K <117919051+nitish-k00@users.noreply.github.com>
2024-07-15 14:56:44 +0530

    Update Login.js

--- client/src/user/component/Login.js
+++ client/src/user/component/Login.js
@@ -91,11 +91,7 @@ function Login() {
             style={{ border: "none", backgroundColor: "white", color: "blue" }}
             onClick={() => navigate("/LogRegDelivery")}
           >
-<<<<<<< HEAD
-            click me
-=======
-          Delivery Account ?
->>>>>>> origin/master
+
           </Button>
           <h1 className="p-3 radius-1 main bg1">WELCOME BACK</h1>
         </div>
